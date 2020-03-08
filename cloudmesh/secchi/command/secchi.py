f94f6621b91358428a3963414e93ed53075fec4d
gitdimishra <dm.alld@gmail.com>
2020-03-08 15:34:59 -0400

    updated few commands

--- cloudmesh/secchi/command/secchi.py
+++ cloudmesh/secchi/command/secchi.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
+from cloudmesh.secchi.Video import Video
 from cloudmesh.secchi.api.manager import Manager
 from cloudmesh.common.console import Console
-from cloudmesh.common.util import path_expand
+from cloudmesh.common.util import path_expand, banner
 from pprint import pprint
 from cloudmesh.common.debug import VERBOSE
+from cloudmesh.shell.command import map_parameters
 
 class SecchiCommand(PluginCommand):
 
@@ -16,14 +18,15 @@ class SecchiCommand(PluginCommand):
         ::
 
           Usage:
-                secchi upload VIDEO [--training]
+                secchi upload [VIDEO] [--training]
                 secchi list [VIDEO]
                 sechhi delete VIDEOS
                 secchi server start
                 secchi server stop
                 secchi server status
-                secchi labeler install
-                secchi labeler run
+                secchi labelImg install
+                secchi labelImg run
+                secchi captureImage
 
           This command does some useful things.
 
@@ -35,19 +38,33 @@ class SecchiCommand(PluginCommand):
 
         """
 
+        map_parameters(arguments,
+                       'training')
+
         VERBOSE(arguments)
 
 
 
 
-        if arguments.upload and arguments["--training"]:
+        if arguments.upload and arguments.training:
             print ("training")
 
+
         if arguments.upload:
+            filename = arguments.VIDEO
             print ("analyse")
+            file = path_expand("~/textfile.txt") #use Pathlib
+            v = Video()
+            v.upload(file)
 
+        if arguments.captureImage:
+            print("capture image from videos for training purpose")
 
-        elif arguments.list:
-            print ("list")
+        if arguments.list:
+            banner("THis is list command")
+            Console.okay("okay")
+            print("list")
 
         return ""
+
+
